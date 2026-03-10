2c90588feb0bd5be5ab4f756ace06e66587c0352
RahulPatil <rahulrohidaspatil@gmail.com>
2026-03-10 17:40:44 +0100

    update title and header to reflect "Telegram Clone"

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center pt-2 font-sans text-2xl">
-      Next.js Template by Rahul Patil
+      Telegram Clone by Rahul Patil
     </div>
   )
 }
--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import { ModeToggle } from "./mode-toggle"
 export default function Header() {
   return (
     <header className="sticky top-0 flex items-center justify-between border-b bg-background p-2">
-      <h1 className="text-2xl">Next.js Template</h1>
+      <h1 className="text-2xl">Telegram Clone</h1>
       <div className="flex items-center justify-between gap-2">
         <ModeToggle />
         <AuthControls />
